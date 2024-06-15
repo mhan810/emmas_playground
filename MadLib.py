86b7ecea68851dc5b0a0af75c6f094cd7600b448
Michael C. Han <mhan810@yahoo.com>
2024-06-15 16:38:17 -0500

    add initial files

---
+++ MadLib.py
@@ -0,0 +1,6 @@
+#MadLib.py
+adjective = input ("Please enter a adjective: ")
+noun = input ("Please enter a noun: ")
+verb = input ("Please enter a verb:")
+print ("Your MadLib:")
+print ("The" , adjective, noun, verb, "over the lazy brown dog.")
---
+++ SquareSpiral1.py
@@ -0,0 +1,6 @@
+#SquareSpiral1 - draw a square spiral
+import turtle
+t = turtle.Pen()
+for x in range(100):
+    t.forward(x)
+    t.left(90)
